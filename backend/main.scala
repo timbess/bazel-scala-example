ac9547c353b240168c4bff282f78af914deb37bb
Timothy Bess <tim@prospective.dev>
2024-05-25 02:46:45 -0400

    wip

--- backend/main.scala
+++ backend/main.scala
@@ -70,7 +70,7 @@ object ActorHierarchyExperiments extends App {
   val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
 
   println(
-    s"Server now online. Please navigate to http://localhost:8080/hello\nPress RETURN to stop..."
+    s"Server now online. Please navigate to http://localhost:8084/hello\nPress RETURN to stop..."
   )
   StdIn.readLine() // let it run until user presses return
   bindingFuture
