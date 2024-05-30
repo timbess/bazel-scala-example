ad41d30a6dd04487094d52e4605c6b422fec8eeb
Timothy Bess <timbessmail@gmail.com>
2024-05-30 01:00:22 -0400

    improve cats example

--- backend/cats/main.scala
+++ backend/cats/main.scala
@@ -14,23 +14,37 @@ import org.http4s.ember.server.EmberServerBuilder
 import org.typelevel.log4cats.LoggerFactory
 import org.typelevel.log4cats.slf4j.Slf4jFactory
 import com.comcast.ip4s._
+import org.http4s.HttpApp
+import cats.effect.kernel.Ref
+import cats.effect.kernel.Sync
 
 object Main extends IOApp.Simple {
   implicit val logging: LoggerFactory[IO] = Slf4jFactory.create[IO]
 
-  def routes[F[_]: Monad]: HttpRoutes[F] = {
+  def routes[F[_]: Monad](ref: Ref[F, Int]): HttpRoutes[F] = {
     val dsl = Http4sDsl[F]
     import dsl._
     HttpRoutes.of[F] { case GET -> Root / "hello" =>
-      Ok("Example")
+      ref.get.flatMap { count =>
+        ref
+          .update(_ + 1) >>
+          Ok(s"Hello, you are visitor number $count")
+      }
     }
   }
+
+  def program[F[_]: Sync]: F[HttpApp[F]] = for {
+    ref <- Ref.of[F, Int](0)
+  } yield routes[F](ref).orNotFound
+
   override def run: IO[Unit] =
-    EmberServerBuilder
-      .default[IO]
-      .withHost(ipv4"0.0.0.0")
-      .withPort(port"8080")
-      .withHttpApp(routes[IO].orNotFound)
-      .build
-      .useForever
+    program[IO].flatMap { app =>
+      EmberServerBuilder
+        .default[IO]
+        .withHost(ipv4"0.0.0.0")
+        .withPort(port"8080")
+        .withHttpApp(app)
+        .build
+        .useForever
+    }
 }
